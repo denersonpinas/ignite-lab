0f71d98deab88a371d002463830b06b43290edf8
denersonpinas <denersonpinas19@gmail.com>
2022-06-22 23:37:35 -0300

    subindo aula 02 do ignite lab, criação do header e sidebar

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,42 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
+import { Event } from "./pages/Event";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      slug
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  slug: string;
-  title: string;
-}
-
-function App() {
-  
-  // useEffect(() => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY,
-  //   }).then(response => {
-  //     console.log(response.data)
-  //   })
-  // })
-
-  // FORMA OTIMIZADA DE FAZER O MESMO QUE A FUNÇÃO ACIMA
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
+export function App() {
 
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <>
+        <Event/>
+    </>
   )
 }
-
-export default App
